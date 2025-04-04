a3213699712d136980c2e41461d7c8364a3906c5
Spygon9 <david_acav@hotmail.com>
2025-04-03 21:20:29 -0600

    Arquitectura inicial con, se agregaron SOPApp.Shared, SOPApp.Web y SOPApp.Mobile

---
+++ SOPApp/SOPApp.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SOPApp.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SOPApp/SOPApp.Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SOPApp.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
